36b97fb53a243150ef5c6ddead1ca0502414c8ad
Vladimir Trifonov <trifonov.dev@gmail.com>
2016-11-27 19:02:35 +0200

    added service discovery(production only)

--- app.js
+++ app.js
@@ -3,6 +3,8 @@
 const express = require('express');
 var app = express();
 
+var Thalassa = require('thalassa');
+
 var env = process.env.NODE_ENV || 'development';
 var config = require('./server/config/config')[env];
 
@@ -10,6 +12,21 @@ require('./server/config/express')(config, app);
 require('./server/config/database')(config);
 require('./server/config/routes')(app);
 
-app.listen(config.port, () => {
-    console.log('Listening on port ' + config.port);
+app.listen(config.port, function () {
+    if (env === 'production') {
+        var client = new Thalassa.Client({
+            apiport: 80,
+            host: process.env.SERVICE_REGISTRY,
+            log: function (i, m) {
+                console.log(m);
+            }
+        });
+
+        client.register('desert-monsters-blog-service', '1.0.0', config.port, {
+            url: process.env.HOST
+        });
+        client.start();
+
+        console.log('Server listens at port:' + config.port);
+    }
 });
